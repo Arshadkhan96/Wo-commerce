5244ed8af45e6cb727fdbd48300759b201e010e6
shiksha <shikshat51@gmail.com>
2024-12-10 20:27:21 +0530

    filter and pagination api made

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,145 +1,128 @@
-const Product = require("../models/productModel")
-const CatchError = require("../resources/catcherror")
-const tryCatcherror = require('../Middleware/tryCatcherror')
-const ApiFeatures =  require("../resources/apiFeatures")
+const Product = require("../models/productModel");
+const CatchError = require("../resources/catcherror");
+const tryCatcherror = require("../Middleware/tryCatcherror");
+const ApiFeatures = require("../resources/apiFeatures");
 
 exports.createProduct = tryCatcherror(async (req, res, next) => {
-  const product = await Product.create(req.body)
+  const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
-    product
-})
-})
+    product,
+  });
+});
 
 exports.findAllProduct = tryCatcherror(async (req, res) => {
-  const apiFeatures = new ApiFeatures(Product.find(),req.query).search();
+  const productPerPage = 5;
+  const totalProduct = await Product.countDocuments();
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(productPerPage);
   const product = await apiFeatures.query;
   res.status(200).json({
-    success:true,
-    product
-  })
-})
+    success: true,
+    product,
+    totalProduct
+  });
+});
 
-exports.updateProduct=tryCatcherror(async(req,res,next)=>{
-  const updateProduct=await Product.updateOne(
-  req.params,
-  {
-      $set:req.body
-  }
-)
-if(!updateProduct){
-  return next(new CatchError("product not found",404))
-}
-res.status(200).json({
-  success:true,
-  message:"Product updated successfully"
-})
-})
-
-exports.deleteProduct=tryCatcherror(async(req,res,next)=>{
-  const deleteProduct=await Product.deleteOne(req.params)
-  if(!deleteProduct){
-    return next(new CatchError("product not found",404))
+exports.updateProduct = tryCatcherror(async (req, res, next) => {
+  const updateProduct = await Product.updateOne(req.params, {
+    $set: req.body,
+  });
+  if (!updateProduct) {
+    return next(new CatchError("product not found", 404));
   }
   res.status(200).json({
-    success:true,
-    message:"Product deleted successfully"
-  })
-})
+    success: true,
+    message: "Product updated successfully",
+  });
+});
 
-exports.findProduct = tryCatcherror(async(req, res,next) => {
-  const product=await Product.findById(req.params.id)
-  if(!product){
-    return next(new CatchError("product not found",404))
+exports.deleteProduct = tryCatcherror(async (req, res, next) => {
+  const deleteProduct = await Product.deleteOne(req.params);
+  if (!deleteProduct) {
+    return next(new CatchError("product not found", 404));
   }
   res.status(200).json({
-    success:true,
-    product
-  })
-})
+    success: true,
+    message: "Product deleted successfully",
+  });
+});
 
-
+exports.findProduct = tryCatcherror(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new CatchError("product not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 ///////////////////new apne wali////
 
-exports.productReview = tryCatcherror(async(req, res,next) => {
-  const {rating,comment,productId} = req.body;
+exports.productReview = tryCatcherror(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
 
-  console.log("Request Body",req.body);
+  console.log("Request Body", req.body);
 
-// Validate productId
-const mongoose = require("mongoose");
-if(!mongoose.Types.ObjectId.isValid(productId)){
-return next ( new  CatchError("Invalid Product ID",400));
-}
+  // Validate productId
+  const mongoose = require("mongoose");
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    return next(new CatchError("Invalid Product ID", 400));
+  }
 
-// Find the product//
-const product = await Product.findById(productId);
-console.log("product not found:",product);
+  // Find the product//
+  const product = await Product.findById(productId);
+  console.log("product not found:", product);
 
-if(!product){
-  return next ( new CatchError("product not found",404));
-}
-// create a review object
+  if (!product) {
+    return next(new CatchError("product not found", 404));
+  }
+  // create a review object
   const review = {
-    user:req.user._id,
-    name:req.user.name,
-    rating:Number(rating),
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
     comment,
   };
 
-  //   // Check if the user already reviewed this product  
-  
-  // __aayega
-  const isReviewed = product.reviews.find(rev=>rev.user.toString()===req.user._id.toString())
+  //   // Check if the user already reviewed this product
 
-  if(isReviewed){
+  // __aayega
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
     //Update the review
-    product.reviews.forEach((rev)=>{
-      if(rev.user.toString() === req.user._id.toString())
-      (rev.rating=rating),(rev.comment=comment)
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString())
+        (rev.rating = rating), (rev.comment = comment);
     });
-  }else{
+  } else {
     //Add a new review
     product.reviews.push(review);
     product.numberOfReviews = product.reviews.length;
   }
 
   //Calculate the average rating
-const totalRatings = product.reviews.reduce((sum,rev)=> sum + rev.rating, 0);
-product.ratings = totalRatings / product.reviews.length;
+  const totalRatings = product.reviews.reduce(
+    (sum, rev) => sum + rev.rating,
+    0
+  );
+  product.ratings = totalRatings / product.reviews.length;
 
-// // Save the product
-await product.save({
-  validateBeforeSave:false});
+  // // Save the product
+  await product.save({
+    validateBeforeSave: false,
+  });
 
   // // Respond with success
   res.status(200).json({
-    success:true,
-    message:"Review added successfully"
+    success: true,
+    message: "Review added successfully",
   });
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
--- backend/resources/apiFeatures.js
+++ backend/resources/apiFeatures.js
@@ -1,20 +1,40 @@
 class ApiFeatures {
-    constructor(query,urlQuery) {
-        this.query = query;
-        this.urlQuery = urlQuery;
-
-    }
-    search() {
-        const keyword = this.urlQuery.keyword
-            ? {
-                name: {
-                    $regex: this.urlQuery.keyword,
-                    $options: 'i'
-                }
-            } : {};
-        this.query = this.query.find({...keyword});
+  constructor(query, urlQuery) {
+    this.query = query;
+    this.urlQuery = urlQuery;
+  }
+  search() {
+    const keyword = this.urlQuery.keyword
+      ? {
+          name: {
+            $regex: this.urlQuery.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+  filter() {
+    const queryCopy = { ...this.urlQuery };
+    // console.log(queryCopy);
+    const garbageValue = ["keyword","page","limit"];
+    garbageValue.forEach((e) => {
+      delete queryCopy[e];
+    });
+    // console.log(queryCopy);
+    this.query = this.query.find(queryCopy);
+    return this;
+  }
+    pagination(productPerPage) {
+        const currentStatusPage = Number(this.urlQuery.page) || 1;
+        // const currentStatusPage = this.urlQuery.page;
+        console.log(currentStatusPage);
+        const skipp = productPerPage * (currentStatusPage - 1); 
+        console.log(skipp)
+        this.query = this.query.limit(productPerPage).skip(skipp);
         return this;
-    };
+  }
 } 
 
 module.exports = ApiFeatures;
